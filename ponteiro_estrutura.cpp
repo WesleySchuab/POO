dc7ae8fc0ce74af57c7afee5287df2daa1cbb3a0
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2025-09-09 22:31:45 -0300

    primeira parte

---
+++ ponteiro_estrutura.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pokemon {
+    string nome;
+    string tipo
+    int nivel;
+    int hp;
+    
+    void aumentarHP( int pontos ){
+        hp += pontos;
+    }
+    
+};
+
+int main()
+{
+	cout << "\n";
+	cout << " Pokemon temos que vencer \n\n ";
+	
+	// Metodo para aumentar os pontos de vida
+	
+	
+	
+	return 0;
+}
